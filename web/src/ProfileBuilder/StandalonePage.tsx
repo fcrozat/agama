aa56d4b386b3decfee5b09bdbb574e14eb2e8fd2
Frederic Crozat <fcrozat@suse.com>
2026-04-27 15:13:29 +0200

    fix: ensure navigation bar stays at the bottom of the screen

---
+++ web/src/ProfileBuilder/StandalonePage.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Masthead,
+  MastheadMain,
+  MastheadBrand,
+  Page as PFPage,
+  PageSection,
+  Title,
+  Toolbar,
+  ToolbarContent,
+  ToolbarGroup,
+  ToolbarItem,
+} from "@patternfly/react-core";
+import { _ } from "~/i18n";
+
+interface StandalonePageProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const StandalonePage: React.FC<StandalonePageProps> = ({ title, children }) => {
+  return (
+    <PFPage
+      style={{ height: "100vh" }}
+      masthead={
+        <Masthead>
+          <MastheadMain>
+            <MastheadBrand>{_("Agama Profile Builder")}</MastheadBrand>
+          </MastheadMain>
+          <Toolbar id="standalone-toolbar">
+            <ToolbarContent>
+              <ToolbarGroup align={{ default: "alignEnd" }}>
+                <ToolbarItem>
+                  <Title headingLevel="h1" size="lg" style={{ color: "white" }}>
+                    {title}
+                  </Title>
+                </ToolbarItem>
+              </ToolbarGroup>
+            </ToolbarContent>
+          </Toolbar>
+        </Masthead>
+      }
+    >
+      <PageSection isFilled padding={{ default: "noPadding" }} style={{ overflow: "hidden" }}>
+        {children}
+      </PageSection>
+    </PFPage>
+  );
+};
+
+export default StandalonePage;
